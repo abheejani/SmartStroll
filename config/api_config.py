1d66fa8bcd0b1fe039d2c74e95ef71585e68dc50
abheejani <abheejani@gmail.com>
2024-10-17 18:26:51 -0500

    adding final api_config file

---
+++ config/api_config.py
@@ -0,0 +1,4 @@
+import os
+# Please ensure you create your own API key here!!! 
+# The following line should look like: API_KEY = os.getenv("GOOGLE_API_KEY", "{YOUR API KEY HERE}")
+API_KEY = os.getenv("GOOGLE_API_KEY")
